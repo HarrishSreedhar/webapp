4099bc1d7d0e36d0f6517bceead0f33664fc35c8
HarrishSreedhar <harrishsreedharen97@gmail.com>
2016-09-21 10:44:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ app.get('/', function (req, res) {
 app.get('/article-one',function(req,res){
       res.sendFile(path.join(__dirname,'article-one.html'));
 });app.get('/article-two',function(req,res){
-     res.send('Article 2two requested...');
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three',function(req,res){
-     res.send('Article 3 threess requested...');
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
