b524c9d384b7fa12050bafc95188ff16c82e3d62
HarrishSreedhar <harrishsreedharen97@gmail.com>
2016-09-21 12:17:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
 console.log('Loaded!');
+var element=document.getElementById('main-text');
+element.innerHTML="HOLLLAAAAAA!!!";
+
+var img=document.getElementById('img');
+img.onclick=function(){
+  img.style.marginLeft='200px';   
+};
