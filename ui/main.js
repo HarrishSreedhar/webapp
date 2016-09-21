7ff5526ef38585a3d3e84de209d8d0c48e479a9c
HarrishSreedhar <harrishsreedharen97@gmail.com>
2016-09-21 20:15:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,10 +3,10 @@ var element=document.getElementById('main-text');
 element.innerHTML="HOLLLAAAAAA!!!";
 
 var img=document.getElementById('imag');
-var marri=0;
+var marginRight=0;
 function moveLeft(){
-     marri=marri+10;
-     img.style.marri=marri+'px'
+     marginRight=marginRight+10;
+     img.style.marginRight=marginRight + 'px'
 }
 img.onclick=function(){
     var interval=setInterval(moveLeft,100);
