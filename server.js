071ce34596b71cb8a9cf6f3c6b3ca3f557e90a73
HarrishSreedhar <harrishsreedharen97@gmail.com>
2016-09-28 15:46:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,10 +114,14 @@ return htmltemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/:articleName',function(req,res){
-var articleName=req.params.articleName;
-      res.send(createtemplate(articles[articleName]));
+app.get('/article-one',function(req,res){
+res.sendFile(path.join(__dirname,'article-one.html')); 
 });
+//app.get('/:articleName',function(req,res){
+//var articleName=req.params.articleName;
+  //    res.send(createtemplate(articles[articleName]));
+//});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
