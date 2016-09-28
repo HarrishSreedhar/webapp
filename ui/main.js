9a4cd30031cf637821b8c2f175ea7290faa195e5
HarrishSreedhar <harrishsreedharen97@gmail.com>
2016-09-28 18:26:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,9 +35,9 @@ img.onclick=function(){
     
 
  //counter
- 
+  var counter=0;
  var button=document.getElementById('counter');
- var counter=0;
+ //var counter=0;
 
 button.onclick=function(){
    // alert('inside count');
