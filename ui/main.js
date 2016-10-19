13c2526252f4017752893daa18fbcc3bc5f508ff
HarrishSreedhar <harrishsreedharen97@gmail.com>
2016-10-19 10:14:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,17 @@
 var but=document.getElementById("counter");
 but.onclick=function(){
-var request=new XMLHttpRequest();
+var xhr=new XMLHttpRequest();
 request.onreadystatechange=function(){
-    if(request.readyState===XMLHttpRequest.DONE){
-        if(request.state===200){
-            var counter=request.responseText;
+    if(xhr.readyState===XMLHttpRequest.DONE){
+        if(xhr.state===200){
+            var counter=xhr.responseText;
           //  counter=JSON.parse(counter);
-            //var sp=document.getElementById("span");
-            //sp.innerHTML=counter.toString();
-        console.log(counter);
+        var sp=document.getElementById("span");
+            sp.innerHTML=counter.toString();
+       
         }
     }
 }
-    request.open('GET','http://harrishsreedhar.imad.hasura-app.io/counter',true);
-    request.send(null);
+    xhr.open('GET','http://harrishsreedhar.imad.hasura-app.io/counter',true);
+    xhr.send(null);
 };
