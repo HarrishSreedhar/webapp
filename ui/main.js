3ca3a3b640b9862a16ebe9c0ea81fa9d18c7a3a1
HarrishSreedhar <harrishsreedharen97@gmail.com>
2016-10-19 10:32:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ request.onreadystatechange=function(){
         if(request.status===200){
           //  var counter=request.responseText;
             //var cr=JSON.parse(counter);
-            var c="holalal;
+            var c="holalal";
             var sp=document.getElementById("span");
         sp.innerHTML=c;       // console.log//(counter)
         }
