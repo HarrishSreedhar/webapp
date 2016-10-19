347377f52d049a874833d2e3b25ee8b77fe73ba7
HarrishSreedhar <harrishsreedharen97@gmail.com>
2016-10-19 10:58:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ request.onreadystatechange=function(){
         if(request.status===200){
           //  alert('stat 200');
             var counter=request.responseText;
-            var cr=JSON.parse(counter);
+           // var cr=JSON.parse(counter);
             var sp=document.getElementById("span");
         sp.innerHTML=cr.toString();
        // console.log(counter)
