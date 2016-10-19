9476b02ea0abca8ee190571f84aa417de33f8c46
HarrishSreedhar <harrishsreedharen97@gmail.com>
2016-10-19 10:52:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var request=new XMLHttpRequest();
 request.onreadystatechange=function(){
     if(request.readyState===XMLHttpRequest.DONE){
         if(request.status===200){
-            alert('stat 200');
+          //  alert('stat 200');
             var counter=request.responseText;
             //var cr=JSON.parse(counter);
             var sp=document.getElementById("span");
@@ -13,6 +13,6 @@ request.onreadystatechange=function(){
         }
     }
 }
-    request.open("GET","http://harrishsreedhar.imad.hasura-app.io/counter",true);
+    request.open("GET","https://www.google.co.in/?gws_rd=ssl",true);
     request.send(null);
 };
