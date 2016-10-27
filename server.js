8f8052f8b7dbbc84c968e9307723d4c844fd3b2b
HarrishSreedhar <harrishsreedharen97@gmail.com>
2016-10-27 18:49:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ return htmltemplate;
 
 }
 var pool = new Pool(config);
-app.get('/test-db',function(req,res){
+app.get('/test',function(req,res){
    pool.query("SELECT * FROM test",function(err,result){
        if(err){
            res.status(500).send(err.toString());
