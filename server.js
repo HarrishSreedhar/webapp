447d56322cefe190104e57f3cb2c58157810c74c
HarrishSreedhar <harrishsreedharen97@gmail.com>
2016-10-27 18:47:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -155,18 +155,16 @@ app.get('/', function (req, res) {
 //app.get('/article-one',function(req,res){
 //res.sendFile(path.join(__dirname,'article-one.html')); 
 //});
-app.get('/article/:articleName',function(req,res){
+app.get('/articles/:articleName',function(req,res){
 
  pool.query("SELECT * FROM articles where title="+req.params.articleName,function(err,result){
      if(err){
          res.status(500).send(err.toString());
-     }
-     else{ 
-     if(result.rows.length===0)
+     } else { 
+         if(result.rows.length===0)
      {
-         res.status(404).send(err.toString());
-     }
-     else
+         res.status(404).send('Article not found');
+     } else
      {
          var articleData=result.rows[0];
            res.send(createtemplate(articleData));
