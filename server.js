651ff624ab4e769409a71ec22220afb043aad589
HarrishSreedhar <harrishsreedharen97@gmail.com>
2016-10-27 19:08:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -157,7 +157,7 @@ app.get('/', function (req, res) {
 //});
 app.get('/articles/:articleName',function(req,res){
 
- pool.query("SELECT * FROM articles where title="+req.params.articleName,function(err,result){
+ pool.query("SELECT * FROM articles where title= '"+req.params.articleName+"'",function(err,result){
      if(err){
          res.status(500).send(err.toString());
      } else { 
